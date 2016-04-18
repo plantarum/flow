3a5b4dd74ace25dbdd19783b36a1561e0399c4b1
Tyler Smith <tyler@plantarum.ca>
2016-04-18 09:28:36 -0400

    revised old code
    revisions to flow-code and flow-models, mostly reductions as the
    features provided in these files are re-structured in flowComponents,
    flowAnalysis etc.

--- flow-rwd/flow-code.R
+++ flow-rwd/flow-code.R
@@ -1,25 +1,27 @@
 library(flowCore)
 library(car)                            # for deltaMethod
+source("flowHist.R")
+source("flow-models.R")
+source("model-utils.R")
 
-lmd1 <- read.FCS("test-data/LMD good/188-15_+_rad_2015-04-20_647.LMD",
-                 dataset = 1, alter.names = TRUE)
-fh1 <- flowHist(lmd1, "FL3.INT.LIN")
+fh1 <- flowHist(FILE =
+                  "test-data/LMD good/188-15_+_rad_2015-04-20_647.LMD",
+                CHANNEL = "FL3.INT.LIN")
+plot(fh1, init = TRUE)
+
+fh1 <- fhAnalyze(fh1)
+plot(fh1, comps = TRUE)
 fh1
-fh1$nls <- fHnls(fh1)
-fh1$model <- SSoneSSC
-plot(fh1)
-fhIntegrate(fh1)
-fhCV(fh1)
 
-lmd2 <- read.FCS("test-data/LMD good/SM239 + R 2014-12-01 852.LMD",
-                 dataset = 1, alter.names = TRUE)
-fh2 <- flowHist(lmd2, "FL3.INT.LIN")
+fh2 <- flowHist(FILE = "test-data/LMD good/SM239 + R 2014-12-01 852.LMD",
+                CHANNEL = "FL3.INT.LIN")                
+
+fhPeakPlot(fh2, window = 20, smooth = 20)
+plot(fh2, init = TRUE)
+
+fh2 <- fhAnalyze(fh2)
+plot(fh2, comps = TRUE)
 fh2
-fh2$nls <- fHnls(fh2)
-fh2$model <- SSoneSSC
-plot(fh2)
-fhIntegrate(fh2)
-fhCV(fh2)
 
 setwd("~/research/techniques/flow/flow-rwd/test-data/camelina_test")
 lmdFiles <- list.files()
@@ -29,15 +31,18 @@ out <- list()
 i <- lmdFiles[1]
 i <- lmdFiles[2]
 i <- lmdFiles[3]
-i <- lmdFiles[4]
+
+
 i <- lmdFiles[6]
+
+## Problem!! intial peaks are not accurate enough after all the smoothing.
+## Need a way to manually select peak positions for this.
+i <- lmdFiles[4]
 
 ## Problem!! only two peaks, one twice the other -- good data?
 i <- lmdFiles[5]
 
-## Singular gradient? Initial values look ok :/
 i <- lmdFiles[7]
-
 i <- lmdFiles[8]
 i <- lmdFiles[9]
 i <- lmdFiles[10]
@@ -46,114 +51,11 @@ i <- lmdFiles[12]
 i <- lmdFiles[13]
 
 
-for(i in lmdFiles){
+fh1 <- flowHist(FILE = i, CHANNEL = "FL3.INT.LIN")
+plot(fh1, init = TRUE)
 
-  raw <- read.FCS(i, dataset = 1, alter.names = TRUE)
-  fhi <- flowHist(raw, "FL3.INT.LIN")
-  print(fhi)
-  plot(fhi, init = TRUE)
-  fhi$nls <- fHnls(fhi)
-  fhi$model <- SSoneSSC
-  plot(fhi)
-  fhi$counts <- fhIntegrate(fhi)
-  fhi$cv <- fhCV(fhi)
-  out[[i]] <- fhi
-}
-
-fl3.trimmed <- exprs(lmd1)[, "FL3.INT.LIN"][exprs(lmd1)[, "FL3.INT.LIN"] <
-                                              max(exprs(lmd1)[, "FL3.INT.LIN"])]
-## Set one more break than the number of channels we want!
-fl3.1024 <- hist(fl3.trimmed, breaks = seq(from = 0, to=1024, by = 1))
-fl3.256 <- hist(fl3.trimmed, breaks = seq(from = 0, to = 1024, by = 4),
-                plot = FALSE)
-fl3.df <- data.frame(x = 1:length(fl3.256$counts),
-                     intensity = fl3.256$counts) 
-
-plot(fl3.df$intensity, type = 'n')
-polygon(x = c(fl3.df$x, 257), y = c(fl3.df$intensity, 0),
-                col = "lightgray", border = NA) 
-
-
-m3test <- nls(intensity ~ SSoneSSC(a1, Ma, Sa, a2, b1, Mb, Sb, b2, SCa,
-                                   intensity = intensity, xx = x),
-              data = fl3.df)
-
-## Complete model
-lines(x = fl3.df$x, y = predict(m3test), col = 2)
-
-## Debris:
-lines(x = fl3.df$x,
-      y = singleCut(SCa = coef(m3test)["SCa"],
-                    intensity = fl3.df$intensity, xx = fl3.df$x),
-      col = 3, lty = 2, lwd = 2)
-##xx <- fl3.df$x                          # awkward!!
-
-## Internal Standard:
-lines(x = fl3.df$x,
-      y = gaussDipA(Ma = coef(m3test)["Ma"], a1 = coef(m3test)["a1"],
-                    Sa = coef(m3test)["Sa"], a2 = coef(m3test)["a2"],
-                    xx = fl3.df$x),
-      col = 4)
-
-## Sample:
-lines(x = fl3.df$x,
-      y = gaussDipB(Mb = coef(m3test)["Mb"], b1 = coef(m3test)["b1"],
-                    Sb = coef(m3test)["Sb"], b2 = coef(m3test)["b2"],
-                    xx = fl3.df$x),
-      col = 5)
-
-
-legend(legend = c("Internal Standard", "Sample", "Debris", "Complete Model"),
-       col = c(5, 4, 3, 2), x = "topright", lty = c(1, 1, 2, 1))
-
-testFun <- function(x){
-  oneSampleSC(xx = x, a1 = coef(m3test)["a1"], Ma = coef(m3test)["Ma"],
-              Sa = coef(m3test)["Sa"], a2 = coef(m3test)["a2"],
-              b1 = coef(m3test)["b1"], Mb = coef(m3test)["Mb"],
-              Sb = coef(m3test)["Sb"], b2 = coef(m3test)["b2"],
-              SCa = coef(m3test)["SCa"], intensity = fl3.df$intensity)
-}
-
-gaussAmodel <- function(x){
-  gaussDipA(xx = x, a1 = coef(m3test)["a1"], Ma = coef(m3test)["Ma"],
-              Sa = coef(m3test)["Sa"], a2 = coef(m3test)["a2"])
-}
-
-
-
-gaussBmodel <- function(x){
-  gaussDipB(xx = x, b1 = coef(m3test)["b1"], Mb = coef(m3test)["Mb"],
-              Sb = coef(m3test)["Sb"], b2 = coef(m3test)["b2"])
-}
-
-modelledEvents <-
-  integrate(testFun, 0, 256, subdivisions = 2000)
-AEvents <- integrate(gaussAmodel, 0, 256, subdivisions = 2000)
-BEvents <- integrate(gaussBmodel, 0, 256, subdivisions = 2000)
-CVa <- coef(m3test)["Sa"]/coef(m3test)["Ma"]
-CVb <- coef(m3test)["Sb"]/coef(m3test)["Mb"]
-CI <- deltaMethod(m3test, "Ma/Mb")
-
-#confint(m3test, "Ma")
-#confint(m3test, "Mb")
-
-modelledEvents
-AEvents
-BEvents
-CVa
-CVb
-c(CI$Estimate - 1.96 * CI$SE, CI$Estimate + 1.96 * CI$SE)
-
-gaussAEvents <-
-  integrate(function (x) coef(m3test)["a1"] / (sqrt(2 * pi) * coef(m3test)["Sa"]) *
-                         exp(-((x - coef(m3test)["Ma"])^2)/(2 *
-                                                        coef(m3test)["Sa"]^2)),
-            0, 256)
-
-gaussBEvents <-
-  integrate(function (x) coef(m3test)["b1"] / (sqrt(2 * pi) * coef(m3test)["Sb"]) *
-                         exp(-((x - coef(m3test)["Mb"])^2)/(2 *
-                                                        coef(m3test)["Sb"]^2)),
-            0, 256)
+fh1 <- fhAnalyze(fh1)
+plot(fh1, comps = TRUE)
+fh1
 
 
--- flow-rwd/flow-models.R
+++ flow-rwd/flow-models.R
@@ -1,122 +1,56 @@
-library(caTools)                        # for runMax
-
-## Single-cut debris model
-## 
-## S(x) = a ⱼ₌ₓ₊₁∑ⁿ ³√j YⱼPₛ(j, x)
-## Pₛ(j, x) = 2 / (πj √(x/j)(1 - x/j)
-##
-## a = amplitude parameter
-## Yⱼ = intensity in channel j
-## Pₛ(j, x) = probability of a nuclei from channel j falling into channel x
-## when cut.
-##
-## By this formula, the debris intensity in a channel/bin is a function of
-## the intensity in all the subsequent bins. This recursive relationship is
-## tricky to code; in order to take full advantage of all of the R tools
-## that support nls, the mean function needs to return one fitted value for
-## one predictor value. The following implementation of singleCut therefore
-## takes the entire vector of the response vector (intensity), necessary to
-## calculate the debris curve, and returns only the value for a single
-## predictor value.
-
-singleCutBase <- function(SCa, intensity, xx){
-  # Don't extend the model below/beyond the data
-  # Modfit appears to cut off the debris slightly above the lowest data,
-  # which gives a better fit. Perhaps set first.channel to 2-4? Need to
-  # test this and determine best fit. Possibly use an extra parameter to
-  # tune this for each data set individually.
-  first.channel <- which(fl3.df$intensity > 0)[2]
-
-  res <- 0
-  if(xx >= first.channel & xx < length(intensity)){
-    channels = (xx + 1):length(intensity)
-    for(j in channels){
-      res <- res + j^(1/3) * intensity[j] * 2 / (pi * j * sqrt(xx/j * (1 - xx/j)))
-    }
-  }
-  return(SCa * res)
+a2b1sc <-
+  function (xx, a1, Ma, Sa, a2, b1, Mb, Sb, SCa, intensity) { 
+    ## a1 == highest G1 peak
+    a1/(sqrt(2 * pi) * Sa) * exp(-((xx - Ma)^2)/(2 * Sa^2)) +
+      ## a2 == G2 peak for a1
+      a2/(sqrt(2 * pi) * Sa * 2) * exp(-((xx - Ma * 2)^2)/(2 * (Sa * 2)^2)) +
+        ## b1 == second highest peak, assumed to be the other G1 peak
+        b1/(sqrt(2 * pi) * Sb) * exp(-((xx - Mb)^2)/(2 * Sb^2)) +
+        ## single cut debris curve
+        singleCut(SCa, intensity, xx) 
 }
 
-## Need to Vectorize this so our comparisons in line 2 make sense. Without
-## this, the comparisons try to test all values of the xx vector against
-## first.channel and length(intensity). This raises a warning, and the
-## results are not what we want. We need them tested one at a time, hence
-## the vectorize here: 
-singleCut <- Vectorize(singleCutBase, "xx")
+a2b1scInit <- function(mCall, LHS, data) {
+  ## Not sure we need this fancy stuff, given we have the data already in
+  ## hand, and in order:
+  
+  xy <- sortedXyData(mCall[["xx"]], LHS, data)
+  ##xy <- data[, "x", "intensity"]
+  window = 20                           # should window be a function
+                                        # argument? 
+  peaks <- findPeaks(xy[, "y"], window)     
 
-findPeaks <- function(dat, window){
-  ## extract all peaks from data
-  localMax <- runmax(dat, k = window)
-  isMax <- localMax == dat
-  maxVals <- dat[isMax]
-  res <- cbind(mean = (1:length(dat))[isMax], height = maxVals)
+  peaks <- cleanPeaks(peaks, window)
 
-  ## remove low peaks (noise). Probably a better threshold than mean could
-  ## be used?
-  res <- res[res[, "height"] > mean(dat), ]
-  res
+  ## ensure A is the peak with the lower mean:
+  peakA <- ifelse(peaks[1, "mean"] < peaks[2, "mean"], 1, 2)
+  peakB <- ifelse(peakA == 1, 2, 1)
+  
+  Ma <- peaks[peakA, "mean"]  
+  Sa <- Ma / 20                         # assume CV = 0.05
+  a1 <- peaks[peakA, "height"] * Sa / 0.4
 
+  ## Is a2 off the chart?
+  if((peaks[peakA, "mean"] * 2) > max(xy[ ,"x"]))
+    a2 <- 0
+  else
+    a2 <- xy[peaks[peakA, "mean"] * 2, "y"] * Sa * 2 / 0.4
+
+  Mb <- peaks[peakB, "mean"]
+  Sb <- Mb / 20
+  b1 <- peaks[peakB, "height"] * Sb / 0.4
+
+  SCa <- 0.1                            # just a wild guess for now
+  value <- c(a1, Ma, Sa, a2, b1, Mb, Sb, SCa)
+  names(value) <-
+    mCall[c("a1", "Ma", "Sa", "a2", "b1", "Mb", "Sb", "SCa")] 
+  value
 }
 
-cleanPeaks <- function(peaks, window){
-  ## Remove ties and multiple peaks for histogram analysis
+SSa2b1sc <- selfStart(a2b1sc, a2b1scInit,
+                      c("a1", "Ma", "Sa", "a2", "b1", "Mb", "Sb", "SCa"))
 
-  ## Screen out any ties - if two peaks have the same height, and are
-  ## within the same 'window', we need to drop one.
-  
-  ## If a peak has a 'match' at half the size, use the smaller peak (ie.,
-  ## take the G1 peak in cases where the G2 peak is higher) 
-
-  ## After the first peak is selected, only consider peaks that are not a
-  ## multiple of the size of this peak when selecting the next one.
-
-  peaks <- peaks[order(peaks[,2], decreasing = TRUE), ]
-
-  ## eliminate the debris field?
-  peaks <- peaks[which(peaks[, "mean"] > 40), ]
-
-  drop <- numeric()
-  for(i in 2: nrow(peaks)){
-    if((peaks[i-1, "height"] == peaks[i, "height"]) &
-       (abs(peaks[i-1, "mean"] - peaks[i, "mean"]) <= window)){ 
-      ## It's a tie!
-      drop <- c(drop, i)
-    }
-  }
-
-  if(length(drop) > 0){                  # there was at least one tie 
-    peaks <- peaks[-drop, ]
-  }
-  
-  out <- matrix(NA, nrow = 0, ncol = 2)
-
-  while(nrow(peaks) > 0){
-    ## which peaks are half or double the size of the first peak:
-    paircheck <-
-      which(((peaks[, "mean"] < 0.53 * peaks[1, "mean"]) &
-             (peaks[, "mean"] > 0.47 * peaks[1, "mean"])) |
-            ((peaks[, "mean"] < 2.13 * peaks[1, "mean"]) &
-             (peaks[, "mean"] > 1.89 * peaks[1, "mean"])))
-    ## Add the first peak to that list:
-    paircheck <- c(1, paircheck)
-    if(length(paircheck) == 1){            # no pairs
-      out <- rbind(out, peaks[1, ])
-      peaks <- peaks[-1, , drop = FALSE]              # remove peak
-    } else {                            # pick the smallest of the pair
-      out <- rbind(out, peaks[paircheck[which.min(peaks[paircheck, "mean"])], ])
-      peaks <- peaks[-paircheck, , drop = FALSE]      # remove pair
-    }
-  }
-
-  if(is.vector(peaks))
-    out <- rbind(out, peaks)
-
-  rownames(out) <- NULL
-  
-  out
-}
-  
-oneSampleSC <-
+a2b2sc <-
   function (xx, a1, Ma, Sa, a2, b1, Mb, Sb, b2, SCa, intensity) { 
     ## a1 == highest G1 peak
     a1/(sqrt(2 * pi) * Sa) * exp(-((xx - Ma)^2)/(2 * Sa^2)) +
@@ -130,7 +64,7 @@ oneSampleSC <-
         singleCut(SCa, intensity, xx) 
 }
 
-oneSampleSCInit <- function(mCall, LHS, data) {
+a2b2scInit <- function(mCall, LHS, data) {
   ## Not sure we need this fancy stuff, given we have the data already in
   ## hand, and in order:
   
@@ -152,8 +86,6 @@ oneSampleSCInit <- function(mCall, LHS, data) {
   else
     a2 <- xy[peaks[1, "mean"] * 2, "y"] * Sa * 2 / 0.4
 
-  peakTwo <- 
-  
   Mb <- peaks[2, "mean"]
   Sb <- Mb / 20
   b1 <- peaks[2, "height"] * Sb / 0.4
@@ -169,7 +101,11 @@ oneSampleSCInit <- function(mCall, LHS, data) {
   value
 }
 
-SSoneSSC <- selfStart(oneSampleSC, oneSampleSCInit,
+SSa2b2sc <- selfStart(a2b2sc, a2b2scInit,
+                      c("a1", "Ma", "Sa", "a2", "b1", "Mb", "Sb", "b2",
+                        "SCa"))  
+
+FSa2b2sc <- selfStart(a2b2sc, flowSS,
                       c("a1", "Ma", "Sa", "a2", "b1", "Mb", "Sb", "b2",
                         "SCa"))  
 
@@ -191,180 +127,7 @@ gaussDipB  <- function(b1, Mb, Sb, b2, xx){
     b2 / (sqrt(2 * pi) * Sb * 2) * exp(-((xx - Mb * 2)^2)/(2 * (Sb * 2)^2))
 }
 
-nlsIntegrate <- function(nlsObj, nlsMod, fcsINT, lower = 0, upper =
-                         256, subdivisions = 1000){
-  do.call(integrate,
-          c(substitute(nlsObj),
-            as.list(coef(nlsMod)),
-            intensity = substitute(fcsINT),
-            lower = lower, upper = upper,
-            subdivisions = subdivisions))
+gaussDipB1  <- function(b1, Mb, Sb, xx){
+  b1 / (sqrt(2 * pi) * Sb) * exp(-((xx - Mb)^2)/(2 * Sb^2))
 }
-
-fhIntegrate <- function(fh, lower = 0, upper = 256, subdivisions = 1000){
-  total <-
-    do.call(integrate,
-            c(substitute(fh$model),
-              as.list(coef(fh$nls)),
-              intensity = substitute(fh$data$intensity),
-              lower = lower, upper = upper,
-              subdivisions = subdivisions))
-  firstPeak <-
-    integrate(peakA, a1 = coef(fh$nls)["a1"],
-              Ma = coef(fh$nls)["Ma"],
-              Sa = coef(fh$nls)["Sa"],
-              lower = lower, upper = upper,
-              subdivisions = 1000)
-  secondPeak <-
-    integrate(peakB, b1 = coef(fh$nls)["b1"],
-              Mb = coef(fh$nls)["Mb"],
-              Sb = coef(fh$nls)["Sb"],
-              lower = lower, upper = upper,
-              subdivisions = 1000)
-              
-  return(list(total = total, firstPeak = firstPeak,
-              secondPeak = secondPeak)) 
-}  
-
-fhCV <- function(fh){
-  CVa <- coef(fh$nls)["Sa"]/coef(fh$nls)["Ma"]
-  CVb <- coef(fh$nls)["Sb"]/coef(fh$nls)["Mb"]
-  CI <- deltaMethod(fh$nls, "Ma/Mb")
-  return(list(CVa = CVa, CVb = CVb, CI = CI))
-}
-
-nlsIntegrate(SSoneSSC, m3test, fl3.df$intensity)
-
-flowHist <- function(FCS, CHANNEL, MODEL= NULL, bins = 256, maxBins = 1024){
-  ## You probably want to subdivide the bins evenly. i.e., if there are
-  ## 1024 bins in the data, use 128, 256, 512 bins
-  if((1024 %% bins) != 0)
-    warning("maxBins is not a multiple of bins!")
-  ## Extract the data channel
-  chanDat <- exprs(FCS)[, CHANNEL]
-
-  ## remove the top bin - this contains clipped values representing all
-  ## out-of-range data, not true values
-  chanTrim <- chanDat[chanDat < max(chanDat)]
-
-  ## aggregate bins: combine maxBins into bins via hist
-  binAg <- floor(maxBins / bins)
-
-  histBins <- hist(chanTrim, breaks = seq(from = 0, to = 1024, by = binAg),
-                   plot = FALSE)
-
-  intensity <- histBins$counts
-
-  res <- list(channel = CHANNEL,
-              data = data.frame(x = 1:length(intensity),
-                                intensity = intensity),
-              model = MODEL, nls = NULL, standard = NULL,
-              file = FCS@description$GUID)
-  class(res) <- "flowHist"
-  
-  return(res)
-}
-              
-print.flowHist <- function(self){
-  message("flowHist object")
-  message("Source file: ", self$file)
-  message(paste("Channel: ", self$channel))
-  message(paste("Values: ", dim(self$data)[1]))
-  message(paste("Total events: ", sum(self$data$intensity)))
-
-  ## if(is.null(self$model)){
-  ##   message("No model selected")
-  ## } else {
-  ##   message(paste("Model: ", substitute(self$model)))
-  ## }
-
-  if(is.null(self$nls)){
-    message("Not fit")
-  } else {
-    ## replace this with some measure of goodness-of-fit
-    message(paste("Fit!"))
-  }
-
-  if(is.null(self$standard)){
-    message("Standard not specified")
-  } else {
-    message(paste("Standard GC value: ", substitute(self$standard)))
-  }
-
-  if(!is.null(self$counts)){
-    message(paste("Modelled events:", round(self$counts$total$value, 1)))
-    message(paste("Peak A:", round(self$counts$firstPeak$value, 1)))
-    message(paste("Peak B:", round(self$counts$secondPeak$value, 1)))
-  }
-
-  if(!is.null(self$cv)){
-    message(paste("CV A:", round(self$cv$CVa, 3)))
-    message(paste("CV B:", round(self$cv$CVb, 3)))
-  }
-
-}
-
-fHnls <- function(fh){
-  nls(intensity ~ SSoneSSC(a1, Ma, Sa, a2, b1, Mb, Sb, b2, SCa,
-                           intensity = intensity, xx = x),
-      data = fh$data)
-}
-
-fHnlsP <- function(fh){
-  nls(intensity ~ SSoneSSC(a1, Ma, Sa, a2, b1, Mb, Sb, b2, SCa,
-                           intensity = intensity, xx = x),
-      algorithm = "port",
-      lower = c(a1 = 0, Ma = 0, Sa = 0, a2 = 0, b1 = 0, Mb = 0, Sb = 0,
-                b2 = 0, SCa = 0),
-      data = fh$data)
-}
-
-fHinitial <- function(fh){
-  getInitial(intensity ~ SSoneSSC(a1, Ma, Sa, a2, b1, Mb, Sb, b2, SCa,
-                                  intensity = intensity, xx = x),
-             data = fh$data)
-}
-
-
-plot.flowHist <- function(self, init = FALSE){
-  plot(self$data$intensity, type = 'n', main = self$file)
-  polygon(x = c(self$data$x, max(self$data$x) + 1), y = c(self$data$intensity, 0),
-          col = "lightgray", border = NA)
-
-  if(init){
-    iv <- fHinitial(self)
-    lines(x = self$data$x,
-          y = SSoneSSC(xx = self$data$x, intensity = self$data$intensity,
-                       a1 = iv["a1"], Ma = iv["Ma"], Sa = iv["Sa"],
-                       a2 = iv["a2"], b1 = iv["b1"], Mb = iv["Mb"],
-                       Sb = iv["Sb"], b2 = iv["b2"], SCa = iv["SCa"]),
-          col = 1, lwd = 3, lty = 5)
-  }
-  if(! is.null(self$nls)){
-    lines(x = self$data$x, y = predict(self$nls), col = 2)
-
-    ## Debris:
-    lines(x = self$data$x,
-          y = singleCut(SCa = coef(self$nls)["SCa"],
-                        intensity = self$data$intensity, xx = self$data$x),
-          col = 3, lty = 2, lwd = 2)
-    ##xx <- self$data$x                          # awkward!!
-
-    ## Internal Standard:
-    lines(x = self$data$x,
-          y = gaussDipA(Ma = coef(self$nls)["Ma"], a1 = coef(self$nls)["a1"],
-                        Sa = coef(self$nls)["Sa"], a2 = coef(self$nls)["a2"],
-                        xx = self$data$x),
-          col = 4)
-
-    ## Sample:
-    lines(x = self$data$x,
-          y = gaussDipB(Mb = coef(self$nls)["Mb"], b1 = coef(self$nls)["b1"],
-                        Sb = coef(self$nls)["Sb"], b2 = coef(self$nls)["b2"],
-                        xx = self$data$x),
-          col = 5)
-
-  }
-}
-
 
